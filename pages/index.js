f632b98e8528bda55b5b3aa28f0fd361b007e4c8
Matheus Reis <105878769+Frouzin@users.noreply.github.com>
2025-03-03 20:34:11 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Amor, eu te amo. se tu também me ama olha pra mim</h1>
+function Home() {
+  return <h1>Amor, eu te amo. se tu também me ama olha pra mim</h1>;
 }
 
-export default Home; 
+export default Home;
